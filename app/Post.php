73fd3bb6aa44f95b612cd0fa06ac333383af6d49
nisha <nishagroup777@gmail.com>
2020-05-25 05:53:56 -0700

    Add all my files

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+     protected $fillable = [
+        'title', 'body'
+    ];
+    public $timestamps= false;
+    public $table='posts';
+
+}
